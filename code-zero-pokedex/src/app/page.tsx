6880cd2dee5c79242b71d9332149e341baf4448a
PauGallego <paugf2004@gmail.com>
2024-11-23 21:27:09 +0100

    Added css modifiers

--- code-zero-pokedex/src/app/page.tsx
+++ code-zero-pokedex/src/app/page.tsx
@@ -1,11 +1,12 @@
 // import Pokedex from '@/components/pokedex'
+import Pokedex from '@/components/pokedex'
 import PokedexView from '@/components/pokedexView'
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 flex items-center bg-[#EE524A] justify-center">
+    <main className="min-h-screen p-4 flex items-center  justify-center">
       {/* <Pokedex /> */}
-      <PokedexView/>
+      <Pokedex/>
     </main>
   )
 }
