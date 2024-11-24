ee8823540c7c3ead347a0d6c5790c029f9c6f9a8
Arnau Briet <arnaubriet@gmail.com>
2024-11-24 03:36:27 +0100

    pokedex view finished

--- code-zero-pokedex/src/app/page.tsx
+++ code-zero-pokedex/src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Pokedex from '@/components/pokedex'
 import Pokedex from '@/components/pokedex'
+import PokedexView from '@/components/pokedexView/pokedexView'
 
 export default function Home() {
   return (
     <main className="min-h-screen  p-4 flex items-center  justify-center">
       {/* <Pokedex /> */}
-      <Pokedex/>
+      {/* <Pokedex/> */}
+      <PokedexView/>
     </main>
   )
 }
